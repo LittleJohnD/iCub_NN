7e1caedb343ce0e9b60c72b4b36fa43840d4b919
John Dyer <jbdyer@gmail.com>
2012-07-05 11:27:43 +0100

    Network class added

---
+++ iCub_NN/Network.cpp
@@ -0,0 +1,18 @@
+/*
+ * Network.cpp
+ *
+ *  Created on: 5 Jul 2012
+ *      Author: little-john
+ */
+
+#include "Network.h"
+
+Network::Network() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Network::~Network() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ iCub_NN/Network.h
@@ -0,0 +1,17 @@
+/*
+ * Network.h
+ *
+ *  Created on: 5 Jul 2012
+ *      Author: little-john
+ */
+
+#ifndef NETWORK_H_
+#define NETWORK_H_
+
+class Network {
+public:
+	Network();
+	virtual ~Network();
+};
+
+#endif /* NETWORK_H_ */
