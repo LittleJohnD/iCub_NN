47e45557dc1ab2d072288f051d9b9b230fa50324
John Dyer <jbdyer@gmail.com>
2012-07-05 11:45:30 +0100

    Skeleton files

---
+++ iCub_NN/Main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 5 Jul 2012
+ *      Author: little-john
+ */
+
+#include "Main.h"
+
+Main::Main() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Main::~Main() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ iCub_NN/Main.h
@@ -0,0 +1,17 @@
+/*
+ * Main.h
+ *
+ *  Created on: 5 Jul 2012
+ *      Author: little-john
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+class Main {
+public:
+	Main();
+	virtual ~Main();
+};
+
+#endif /* MAIN_H_ */
